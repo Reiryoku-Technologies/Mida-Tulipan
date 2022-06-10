a0e46192d9ee80ee97b18a3c7db32aba96d8135a
Ahsan <m.ahsan9981@gmail.com>
2022-06-10 20:51:23 +0500

    pivot points standard and fibonacci functionality.

---
+++ src/core/indicators/pivot-point/PivotPointParameters.ts
@@ -0,0 +1,75 @@
+/*
+ * Copyright Reiryoku Technologies and its contributors, www.reiryoku.com, www.mida.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+*/
+
+export enum Type {
+    STANDARD,
+    WOODIE,
+    CAMARILLA,
+    DEMARK,
+    FIBONACCI
+};
+
+
+export class PivotPoints  {
+    _r1?: number;
+    _r2?: number;
+    _r3?: number;
+    _r4?: number;
+    _pp?: number;
+    _s1?: number;
+    _s2?: number;
+    _s3?: number;
+    _s4?: number;
+
+    constructor(r1: number, r2: number, r3: number, r4: number, pp: number, s1: number, s2: number, s3: number, s4: number) {
+
+        this._r1 = r1;
+        this._r2 = r2;
+        this._r3 = r3;
+        this._r4 = r4;
+        this._pp = pp;
+        this._s1 = s1;
+        this._s2 = s2;
+        this._s1 = s3;
+        this._s4 = s4;
+    }
+}
+
+export class Candle {
+    _open?: number;
+    _high?: number;
+    _low?: number;
+    _close?: number;
+
+    constructor(open: number, high: number, low: number, close: number) {
+     
+        this._open = open;
+        this._high = high;
+        this._low = low;
+        this._close = close;
+    }
+}
+
+export type PivotPointParameters = {
+    type?: Type;
+    candle?: Candle;
+};
---
+++ src/core/indicators/pivot-point/pivotpoint.ts
@@ -0,0 +1,139 @@
+/*
+ * Copyright Reiryoku Technologies and its contributors, www.reiryoku.com, www.mida.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+*/
+import { PivotPointParameters, Type, PivotPoints, Candle } from "#indicators/pivot-point/PivotPointParameters";
+
+const DEFAULT_PIVOT_POINT_TYPE: Type = Type.STANDARD;
+const DEFAULT_PRICE = 0;
+
+
+export class PivotPoint  {
+
+    readonly #type: Type;
+    readonly #open: number;
+    readonly #high: number;
+    readonly #low: number;
+    readonly #close: number;
+
+    constructor({type, candle}: PivotPointParameters) {
+
+        this.#type = type ?? DEFAULT_PIVOT_POINT_TYPE;
+        this.#open = candle?._open ?? DEFAULT_PRICE;
+        this.#high = candle?._high ?? DEFAULT_PRICE;
+        this.#low  = candle?._low  ?? DEFAULT_PRICE;
+        this.#close = candle?._close ?? DEFAULT_PRICE;
+    }
+
+    public async calculate(pivotPointParameters: PivotPointParameters): Promise<PivotPoints> {
+
+        return new Promise((resolve, reject) => {
+
+            try {
+
+                if (
+                    pivotPointParameters?.candle!._open! <= 0 || 
+                    pivotPointParameters?.candle!._high! <= 0 || 
+                    pivotPointParameters?.candle!._low!  <= 0 ||
+                    pivotPointParameters?.candle!._close! <= 0 
+                   ) {
+
+                    throw new Error("Invalid candle data for pivot point calculation.")
+                }
+
+
+                switch (pivotPointParameters.type) {
+
+                    case Type.STANDARD:
+                        return this.CalculateStandard(pivotPointParameters?.candle!)
+                    case Type.WOODIE:
+                        return this.CalculateWoodie(pivotPointParameters.candle!)
+    
+                    case Type.CAMARILLA:
+                        return this.CalculateCamarilla(pivotPointParameters.candle!)
+    
+                    case Type.DEMARK:
+                        return this.CalculateDemark(pivotPointParameters.candle!)
+
+                    case Type.FIBONACCI:
+                        return this.CalculateFibonacci(pivotPointParameters.candle!)
+                
+                    default:
+                        return this.CalculateStandard(pivotPointParameters.candle!)
+                }
+                
+            } catch (error) {
+                console.log('An error occured.');
+            }
+        });
+
+    }
+
+    private CalculateStandard(candle: Candle): PivotPoints {
+
+        const pp = +((candle._high! + candle._low! + candle._close!) / 3).toFixed(4);
+        const r1 = +((2 * pp) - candle._low!).toFixed(4);
+        const s1 = +((2 * pp) - candle._high!).toFixed(4);
+
+        const r2 = +(pp + (candle._high! - candle._low!)).toFixed(4);
+        const s2 = +(pp - (candle._high! - candle._low!)).toFixed(4);
+
+        const r3 = +(candle._high! + 2 * (pp - candle._low!)).toFixed(4);
+        const s3 = +(candle._low! - 2 *(candle._high! - pp)).toFixed(4);;
+
+
+        return new PivotPoints(r1, r2, r3, 0,  pp, s1, s2, s3, 0);
+
+    }
+
+    private CalculateFibonacci(candle: Candle) {
+
+        const pp = +((candle._high! + candle._low! + candle._close!) / 3).toFixed(4);
+        const r1 = +(pp + (candle._high! - candle._low!) * 0.3820).toFixed(4);
+        const s1 = +(pp - (candle._high! - candle._low!) * 0.3820).toFixed(4);
+
+        const r2 = +(pp + (candle._high! - candle._low!) * 0.6180).toFixed(4);
+        const s2 = +(pp - (candle._high! - candle._low!) * 0.6180).toFixed(4);
+
+
+        const r3 = +(pp + (candle._high! - candle._low!) * 1.0000).toFixed(4);
+        const s3 = +(pp - (candle._high! - candle._low!) * 1.0000).toFixed(4);;
+
+
+        return new PivotPoints(r1, r2, r3, 0, pp, s1, s2, s3, 0);
+
+    }
+
+    private CalculateWoodie(candle: Candle) {
+
+    }
+
+    
+    private CalculateCamarilla(candle: Candle) {
+        
+    }
+
+    private CalculateDemark(candle: Candle) {
+        
+    }
+
+
+
+}
